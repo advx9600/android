363d6efa15cea66d1ed7ab4865de3fd0f4d28fa9
abc <abc@abc-virtual-machine.(none)>
2014-08-18 16:03:08 +0800

    APK反编译

---
+++ tools/java_dissemble/smail/smail_cmd.sh
@@ -0,0 +1,2 @@
+java -jar baksmali-2.0.3.jar -a 12 -x Settings.odex  -d .
+java -jar smali-2.0.3.jar out/ -o classes.dex
---
+++ tools/xml_dissemble/apktools/apktool_cmd.sh
@@ -0,0 +1,2 @@
+java -jar apktool.jar if framework/framework-res.apk --frame-path framework
+java -jar apktool.jar d Settings.apk
